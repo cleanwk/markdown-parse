4365ebcb1ab03feff9aeecbdbaa9de8ac1567003
wukai <wuk.jlu@outlook.com>
2021-11-23 13:05:49 +0800

    文档解析为list string 完成

--- src/main/java/com/example/MdParser.java
+++ src/main/java/com/example/MdParser.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.common.SyntaxTypeEnum;
+import com.example.utils.FileProcessUtil;
 import lombok.Data;
 
 import java.io.File;
@@ -27,12 +28,13 @@ public class MdParser implements Parse{
     public File parseMarkDown(File document) {
 
         //将md文档按照行 转为string list
-
+        mdList = FileProcessUtil.toStringList(document);
         //确定多行语句区块,进行标记
 
         //确定单行语句区块进行标记
 
         //输出为html文档
+
         return null;
     }
 }
--- src/main/java/com/example/utils/FileProcessUtil.java
+++ src/main/java/com/example/utils/FileProcessUtil.java
@@ -1,6 +1,7 @@
 package com.example.utils;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileProcessUtil {
@@ -10,6 +11,54 @@ public class FileProcessUtil {
      * @return
      */
     public static List<String> toStringList(File file){
+        List<String> mdList = new ArrayList();
+        InputStreamReader reader = null;
+        BufferedReader bufferedReader = null;
+
+        while (true){
+            try {
+                reader = new InputStreamReader(new FileInputStream(file));
+                bufferedReader = new BufferedReader(reader);
+                String line = "";
+                while (!((line = bufferedReader.readLine()) != null)) break;{
+                    mdList.add(line);
+                }
+            } catch (IOException e) {
+                System.out.println("------>>>解析markdown 文本出错");
+                e.printStackTrace();
+            }finally {
+                if (reader!=null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        System.out.println("------>>> reader close failed！");
+                        e.printStackTrace();
+                    }
+                }
+
+                if (bufferedReader!=null){
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        System.out.println("------>>> bufferedReader close failed！");
+                        e.printStackTrace();
+                    }
+                }
+
+                return mdList;
+            }
+
+        }
+
+
+    }
+
+    /**
+     * 将标记好的字符串list转换成html文件输出
+     * @param list
+     * @return
+     */
+    public static File toStringList(List list){
 
         return null;
     }
